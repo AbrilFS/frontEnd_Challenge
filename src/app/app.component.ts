96ef74ab32471a8b6bdbde23ce0a675596af10b7
Abril Flores Silva <abril.qfs@gmail.comgit remote add origin git@github.com:AbrilFS/preworkMAC.gitgithhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhellohow areyoudoingtoday!>
2023-02-11 14:10:09 -0600

    Elabore el componente de nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'frontEnd_Challenge';
+  src = 'https://images.unsplash.com/photo-1673660199160-8b4c4f988347?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NjE0MjcwNQ&ixlib=rb-4.0.3&q=80&w=1080';
+  src2 = 'https://images.unsplash.com/photo-1676010101557-34dcdbe5c3ea?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwxfDB8MXxyYW5kb218MHx8fHx8fHx8MTY3NjE0MTY2Mg&ixlib=rb-4.0.3&q=80&w=1080'
 }
